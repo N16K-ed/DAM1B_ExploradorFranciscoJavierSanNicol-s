7245b36dbccaba38f97b3e993ce14b3c1df64472
Fran-casa <fsannicceb@educacion.navarra.es>
2025-01-07 16:15:41 +0100

    clase enemigo sin terminar
    al moverse no detecta ni otros enemigos ni trampas

--- DAM1B_ExploradorFranciscoJavierSanNicolas/src/Enemigo.java
+++ DAM1B_ExploradorFranciscoJavierSanNicolas/src/Enemigo.java
@@ -1,16 +1,37 @@
 public class Enemigo {
+    private final int ARRIBA = 1; //direccion
+    private final int ABAJO = 2; //direccion
+    private final int DERECHA = 3; //direccion
+    private final int IZQUIERDA = 4; //direccion
+
     private Posicion posicionActual;
 
     public Enemigo(Posicion Posicion){
-
+        this.posicionActual = Posicion;
     }
 
     public void Moverse (int direccion){
-
+        if (direccion == ARRIBA){
+            if (this.posicionActual.getCoordenadaFila() != 0){
+                this.posicionActual.setCoordenadaFila(this.posicionActual.getCoordenadaFila() - 1);
+            }
+        }else if (direccion == ABAJO){
+            if (this.posicionActual.getCoordenadaFila() != 5){
+                this.posicionActual.setCoordenadaFila(this.posicionActual.getCoordenadaFila() + 1);
+            }
+        }else if (direccion == DERECHA){
+            if (this.posicionActual.getCoordenadaCol() != 0){
+                this.posicionActual.setCoordenadaCol(this.posicionActual.getCoordenadaCol() - 1);
+            }
+        }else {
+            if (this.posicionActual.getCoordenadaCol() != 19){
+                this.posicionActual.setCoordenadaCol(this.posicionActual.getCoordenadaCol() + 1);
+            }
+        }
     }
 
     public void setPosicionActual(Posicion posicionActual){
-
+        this.posicionActual = posicionActual;
     }
 
     public Posicion getPosicionActual(){
