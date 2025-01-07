442f39b140cf089781b51e945d6c71ed1d665d67
Fran-casa <fsannicceb@educacion.navarra.es>
2025-01-07 19:06:33 +0100

    mapa muestra jugador enemigo y trampas
    tambien se inicializa el tesoro

--- DAM1B_ExploradorFranciscoJavierSanNicolas/src/Main.java
+++ DAM1B_ExploradorFranciscoJavierSanNicolas/src/Main.java
@@ -1,12 +1,31 @@
+import java.util.Scanner;
+
 public class Main {
+    private final int ARRIBA = 1;
+    private final int ABAJO = 2;
+    private final int DERECHA = 3;
+    private final int IZQUIERDA = 4;
+
     public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
         System.out.println("*******************************************************");
         System.out.println("Bienvenido al juego del explorador");
         System.out.println("*******************************************************");
         Mapa mapajuego = new Mapa();
-        do{
+
             mapajuego.Mostrar();
-        }while ()
+
+            System.out.println("\n W=Arriba    A=Izquierda    S=Abajo   D=Derecha\n");
+            System.out.println("¿Qué acción quieres realizar? ");
+            String opcion = teclado.nextLine();
+            while (!(opcion.equalsIgnoreCase("W") || opcion.equalsIgnoreCase("A") ||opcion.equalsIgnoreCase("S") ||opcion.equalsIgnoreCase("D")) ){
+                System.out.println("Introduzca una opción válida");
+                opcion = teclado.nextLine();
+            }
+            int opcionDireccion;
+           // if
+
 
     }
 }
--- DAM1B_ExploradorFranciscoJavierSanNicolas/src/Mapa.java
+++ DAM1B_ExploradorFranciscoJavierSanNicolas/src/Mapa.java
@@ -26,13 +26,21 @@ public class Mapa {
         //inicializar los enemigos
         this.listadoEnemigos = new Enemigo[3];
         for (int i = 0; i < listadoEnemigos.length; i++ ){
-            listadoEnemigos[i] = new Enemigo(new Posicion((int)(Math.random()*6),(int)(Math.random()*19 +1))); //random*19 + 1 para que no aparezca en la columna en la que aparece el explorador
+            do {
+                this.listadoEnemigos[i] = new Enemigo(new Posicion((int) (Math.random() * 6), (int) (Math.random() * 20)));
+            }while (this.listadoEnemigos[i].getPosicionActual() == posJugador ); //do while para que no aparezca en la posición en la que aparece el explorador
         }
         //inicializar las trampas
         this.posicionTrampas = new Posicion[3];
         for (int i = 0; i < posicionTrampas.length; i++ ){
-            posicionTrampas[i] = new Posicion((int)(Math.random()*6),(int)(Math.random()*19 +1));
-        }
+            do {
+                posicionTrampas[i] = new Posicion((int) (Math.random() * 6), (int) (Math.random() * 19 + 1));
+            }while(posicionTrampas[i] == posJugador || posicionTrampas[i] == this.listadoEnemigos[0].getPosicionActual() || posicionTrampas[i] == this.listadoEnemigos[1].getPosicionActual() || posicionTrampas[i] == this.listadoEnemigos[2].getPosicionActual());
+        } // la condicion del do while evita que se posicionen trampas donde los enemigos y jugador
+        //inicializa tesoro
+        do {
+            this.posTesoro = new Posicion((int) (Math.random() * 6), (int) (Math.random() * 20));
+        }while (this.posTesoro == posJugador || this.posTesoro == posicionTrampas[0] || this.posTesoro == posicionTrampas[1] || this.posTesoro == posicionTrampas[2] || posTesoro == this.listadoEnemigos[0].getPosicionActual() || this.posTesoro == this.listadoEnemigos[1].getPosicionActual() || this.posTesoro == this.listadoEnemigos[2].getPosicionActual());
     }
 
     public char[][] getTablero(){
@@ -47,11 +55,19 @@ public class Mapa {
         for (int filas = 0; filas < tablero.length; filas++){
             System.out.println("---------------------------------------------------------------------------------");
             System.out.print("| ");
+            int numeroTrampas;
+            int numeroEnemigos;
             for (int columnas = 0; columnas < tablero[filas].length; columnas++){
-                if (tablero[filas][columnas] == 'J'){ //para los colores de los carácteres
-                    System.out.println("\u001B[32m" + tablero[filas][columnas] + "\u001B[0m" + "|" ); //accede al código ANSI para el color verde (32) y predeterminado (0)
-                }else if (tablero[filas][columnas] == 'E' || tablero[filas][columnas] == 'T'){
-                    System.out.println("\u001B[31m" + tablero[filas][columnas] + "\u001B[0m" + "|" ); // 31 es el valor del rojo
+                if (filas == posJugador.getCoordenadaFila() && columnas == posJugador.getCoordenadaCol()){ //para los colores de los carácteres
+                    System.out.print("\u001B[32m" + "J" + "\u001B[0m" + " | " ); //accede al código ANSI para el color verde (32) y predeterminado (0)
+                }else if ((filas == listadoEnemigos[0].getPosicionActual().getCoordenadaFila() && columnas == listadoEnemigos[0].getPosicionActual().getCoordenadaCol()) ||
+                          (filas == listadoEnemigos[1].getPosicionActual().getCoordenadaFila() && columnas == listadoEnemigos[1].getPosicionActual().getCoordenadaCol()) ||
+                          (filas == listadoEnemigos[2].getPosicionActual().getCoordenadaFila() && columnas == listadoEnemigos[2].getPosicionActual().getCoordenadaCol())){
+                    System.out.print("\u001B[31m" + "E" + "\u001B[0m" + " | " ); // 31 es el valor del rojo
+                }else if ((filas == posicionTrampas[0].getCoordenadaFila() && columnas == posicionTrampas[0].getCoordenadaCol()) ||
+                          (filas == posicionTrampas[1].getCoordenadaFila() && columnas == posicionTrampas[1].getCoordenadaCol()) ||
+                          (filas == posicionTrampas[2].getCoordenadaFila() && columnas == posicionTrampas[2].getCoordenadaCol())){
+                    System.out.print("\u001B[31m" + "T" + "\u001B[0m" +" | " );
                 }else{
                     System.out.print(tablero[filas][columnas] + " | ");
                 }
