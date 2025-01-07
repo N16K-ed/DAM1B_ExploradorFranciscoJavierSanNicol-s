b6c44c04568f933a2ef1a36591534c45f6e804d3
Fran-casa <fsannicceb@educacion.navarra.es>
2025-01-07 17:19:36 +0100

    pruebas de muestra del mapa en el main
    primera "versión" de la clase mapa y prueba en el main del método Mostrar()

--- DAM1B_ExploradorFranciscoJavierSanNicolas/src/Main.java
+++ DAM1B_ExploradorFranciscoJavierSanNicolas/src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("*******************************************************");
+        System.out.println("Bienvenido al juego del explorador");
+        System.out.println("*******************************************************");
+        Mapa mapajuego = new Mapa();
+        do{
+            mapajuego.Mostrar();
+        }while ()
 
     }
 }
--- DAM1B_ExploradorFranciscoJavierSanNicolas/src/Mapa.java
+++ DAM1B_ExploradorFranciscoJavierSanNicolas/src/Mapa.java
@@ -1,16 +1,42 @@
+import java.util.Scanner;
 public class Mapa {
-    private char[][] tablero = new char[6][20];
+    private char[][] tablero;;
     private Posicion posTesoro;
     private Posicion posJugador;
-    private Enemigo[] listadoEnemigos = new Enemigo[3];
-    private Posicion[] posicionTrampas = new Posicion[3];
+    private Enemigo[] listadoEnemigos;
+    private Posicion[] posicionTrampas;
 
     public Mapa(){
+        //Scanner teclado
+        Scanner teclado = new Scanner(System.in);
 
+        // Crea al explorador
+        System.out.println("Introduzca su nombre:");
+        String nombre = teclado.nextLine();
+        Explorador jugador = new Explorador(nombre);
+        this.posJugador = jugador.getPosicionActual();
+
+        //inicializa tablero
+        this.tablero = new char[6][20];
+        for (int filas = 0; filas < tablero.length; filas++){
+            for (int columnas = 0; columnas < tablero[filas].length; columnas++){
+                tablero[filas][columnas] = ' ';
+            }
+        }
+        //inicializar los enemigos
+        this.listadoEnemigos = new Enemigo[3];
+        for (int i = 0; i < listadoEnemigos.length; i++ ){
+            listadoEnemigos[i] = new Enemigo(new Posicion((int)(Math.random()*6),(int)(Math.random()*19 +1))); //random*19 + 1 para que no aparezca en la columna en la que aparece el explorador
+        }
+        //inicializar las trampas
+        this.posicionTrampas = new Posicion[3];
+        for (int i = 0; i < posicionTrampas.length; i++ ){
+            posicionTrampas[i] = new Posicion((int)(Math.random()*6),(int)(Math.random()*19 +1));
+        }
     }
 
-    public int[][] getTablero(){
-        return null;
+    public char[][] getTablero(){
+        return tablero;
     }
 
     public Posicion getPosTesoro(){
@@ -18,6 +44,20 @@ public class Mapa {
     }
 
     public void Mostrar(){
-
+        for (int filas = 0; filas < tablero.length; filas++){
+            System.out.println("---------------------------------------------------------------------------------");
+            System.out.print("| ");
+            for (int columnas = 0; columnas < tablero[filas].length; columnas++){
+                if (tablero[filas][columnas] == 'J'){ //para los colores de los carácteres
+                    System.out.println("\u001B[32m" + tablero[filas][columnas] + "\u001B[0m" + "|" ); //accede al código ANSI para el color verde (32) y predeterminado (0)
+                }else if (tablero[filas][columnas] == 'E' || tablero[filas][columnas] == 'T'){
+                    System.out.println("\u001B[31m" + tablero[filas][columnas] + "\u001B[0m" + "|" ); // 31 es el valor del rojo
+                }else{
+                    System.out.print(tablero[filas][columnas] + " | ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println("---------------------------------------------------------------------------------");
     }
 }
