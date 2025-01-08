5a281f6845f7f54527d226ff045fe0ea962be09b
Fran-casa <fsannicceb@educacion.navarra.es>
2025-01-08 01:39:07 +0100

    clase del EnemigoInteligente y aparece en el mapa
    copia de la clase enemigo

---
+++ DAM1B_ExploradorFranciscoJavierSanNicolas/src/EnemigoInteligente.java
@@ -0,0 +1,41 @@
+public class EnemigoInteligente {
+    private final int ARRIBA = 1; //direccion
+    private final int ABAJO = 2; //direccion
+    private final int DERECHA = 3; //direccion
+    private final int IZQUIERDA = 4; //direccion
+
+    private Posicion posicionActual;
+
+    public EnemigoInteligente(Posicion Posicion){
+        this.posicionActual = Posicion;
+    }
+
+    public void moverse(int direccion){
+        Posicion posJugador = Main.getJugador().getPosicionActual();
+        if (direccion == ARRIBA){
+            if (this.posicionActual.getCoordenadaFila() != 0){
+                this.posicionActual.setCoordenadaFila(this.posicionActual.getCoordenadaFila() - 1);
+            }
+        }else if (direccion == ABAJO){
+            if (this.posicionActual.getCoordenadaFila() != 5){
+                this.posicionActual.setCoordenadaFila(this.posicionActual.getCoordenadaFila() + 1);
+            }
+        }else if (direccion == DERECHA){
+            if (this.posicionActual.getCoordenadaCol() != 0){
+                this.posicionActual.setCoordenadaCol(this.posicionActual.getCoordenadaCol() - 1);
+            }
+        }else {
+            if (this.posicionActual.getCoordenadaCol() != 19){
+                this.posicionActual.setCoordenadaCol(this.posicionActual.getCoordenadaCol() + 1);
+            }
+        }
+    }
+
+    public void setPosicionActual(Posicion posicionActual){
+        this.posicionActual = posicionActual;
+    }
+
+    public Posicion getPosicionActual(){
+        return posicionActual;
+    }
+}
